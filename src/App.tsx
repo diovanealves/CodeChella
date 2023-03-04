2ad6cdca12ddb315cfff166e93f85af3d0a70e1d
Diovane Alves <alvdiovane@gmail.com>
2023-03-03 23:24:14 -0300

    Iniciando Pagina de Experiencia e criando Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Home } from "./pages/Home";
 
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Routes/AllPages";
+
 export function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 }
--- src/pages/Experience/index.tsx
+++ src/pages/Experience/index.tsx
@@ -1,3 +1,9 @@
+import BannerElement from "../../assets/BannerExperience.svg";
+
 export function Experience() {
-  return <></>;
+  return (
+    <div>
+      <img src={BannerElement} alt="" />
+    </div>
+  );
 }
